b2675d14d22b8fdd454a10ee5353be354a1845b1
Haroon Khan <hkhan@healthexpense.com>
2018-01-20 17:50:25 -0800

    Reading Section on Pattern Matching
    - Added Module on Pattern Matching, in cabal
    - Initial Commit for the module
    - Added a module for Data, containing common data definitions
    - Added some more functions

---
+++ haskell/chapter02/src/Chapter02/PatternMatching.hs
@@ -0,0 +1,110 @@
+module Chapter02.PatternMatching where
+
+import Chapter02.Data
+
+-- simple patterns
+clientName :: Client -> String
+clientName client  = case client of
+                       GovOrg name -> name
+                       Company name id person resp -> name
+                       Individual person ads ->
+                         case person of Person fname lname gender -> fname ++ " " ++ lname
+
+
+clientName2 :: Client -> String
+clientName2 client = case client of
+                       GovOrg name -> name
+                       Company name id person resp -> name
+                       Individual (Person fname lname _) _ -> fname ++ " " ++ lname
+
+-- throws an exception, when there is no match
+companyName :: Client -> String
+companyName client = case client of Company name _ _ _ -> name
+
+
+companyName2 :: Client -> Maybe String
+companyName2 client = case client of
+                       Company name _ _ _ -> Just name
+                       _                  -> Nothing
+
+fibonacci :: Integer -> Integer
+fibonacci n = case n of
+                0 -> 0
+                1 -> 1
+                _ -> fibonacci (n-1) + fibonacci (n-2)
+
+f :: Client -> String
+f client = case client of
+             Company _ _ (Person name _ _) "Boss" -> name ++ " is the boss."
+             _                                    -> "There is no boss"
+
+
+-- Causes an exception
+g :: Client -> String
+g client = case client of
+             Company _ _ (Person name _ _ ) pos ->
+               case pos of "Boss" -> name ++ " is the boss."
+             _                                     -> "There is no boss."
+
+
+clientName3 (GovOrg name) = name
+clientName3 (Company name _ _ _) = name
+clientName3 (Individual (Person fname lname _) _) = fname ++ " " ++ lname
+
+fibonacci2 0 = 0
+fibonacci2 1 = 1
+fibonacci2 n = fibonacci2 (n-1) + fibonacci2 (n-2)
+
+-- Excercise 2-5 Perfect Match for your Time Machines
+countGenders::[Client]->(Integer, Integer, Integer)
+countGenders [] = (0,0,0)
+countGenders (hd:tl) = let (maleCount, femaleCount, otherCount) = countGenders tl
+                           countGender' :: Gender -> (Integer, Integer, Integer)
+                           countGender' Male = (maleCount+1, femaleCount, otherCount)
+                           countGender' Female = (maleCount, femaleCount+1, otherCount)
+                           countGender' Other = (maleCount, femaleCount, otherCount)
+                       in case hd of
+                            GovOrg _ -> (maleCount, femaleCount, otherCount)
+                            Company _ _ (Person _ _ gender) _ -> countGender' gender
+                            Individual (Person _ _ gender)  _ -> countGender' gender
+
+-- Excercise 2-5
+discountTimeMachines :: [TimeMachine] -> Double -> [TimeMachine]
+discountTimeMachines [] _ = []
+discountTimeMacines ((TimeMachine manf model name dir p):tl) discount  = (TimeMachine manf model name dir (p*discount)): (discountTimeMachines tl discount)                                                                        
+
+
+--
+
+sorted :: [Integer] -> Bool
+sorted [] = True
+sorted [_] = True
+sorted (x:y:zs) = x < y && sorted (y:zs)
+
+sorted2 [] = True
+sorted2 [_] = True
+sorted2 (x: r @ (y:zs)) = x < y && sorted r
+
+maxmin [x] = (x,x)
+maxmin (x:xs) = ( if x > xs_max then x else xs_max
+                , if x < xs_min then x else xs_min)
+                where (xs_max, xs_min) = maxmin xs
+
+--
+
+ifibonacci :: Integer -> Maybe Integer
+ifibonacci n = if n < 0
+               then Nothing
+               else case n of
+                      0 -> Just 0
+                      1 -> Just 1
+                      n' -> let Just f1 = ifibonacci (n'-1)
+                                Just f2 = ifibonacci (n'-2)
+                            in
+                              Just (f1+f2)
+--
+
+binom _ 0        = 1
+binom x y | x==y = 1
+binom n k        = (binom (n-1) (k-1)) + (binom (n-1) k)
+
--- haskell/chapter02/src/Chapter02/SimpleFunctions.hs
+++ haskell/chapter02/src/Chapter02/SimpleFunctions.hs
@@ -1,5 +1,7 @@
 module Chapter02.SimpleFunctions where
 
+import Chapter02.Data
+
 firstOrEmpty lst = if not (null lst) then head lst else "empty"
 
 maxmin list = if (null (tail list))
@@ -13,18 +15,6 @@ maxmin list = if (null (tail list))
 
 
 
-data Client = GovOrg String
-            | Company String Integer String String
-            | Individual Person Bool
-            deriving Show
-
-
-data Person = Person String String Gender
-              deriving Show
-
--- Ex 2-4
-
-data Gender = Male | Female | Other deriving Show
 
 -- Examples
 p1 = Person "John" "Doe" Male
