34dd2ed8622747869929b75084eb28c3b8cd273b
Haroon Khan <hkhan@healthexpense.com>
2018-02-02 08:48:18 -0800

    Converted project to Library
    Update Library
    Add Unit Tests (Initial commit)

--- projecthaskell/chapter15/src/Chapter15/BinaryTrees.hs
+++ projecthaskell/chapter15/src/Chapter15/BinaryTrees.hs
@@ -1,9 +1,30 @@
+-- | Simple Implementation of Binary Trees
 module Chapter15.BinaryTrees where
 
-data BinaryTree a = Node a (BinaryTree a) (BinaryTree a)
-                  | Leaf 
-                  deriving Show
+-- | * A typical Binary Tree, The main Binary Tree
+data BinaryTree a = Node a (BinaryTree a) (BinaryTree a) -- ^Inner Nodes
+                  | Leaf                                 -- ^Leaves
+                  deriving (Show,Eq)
 
+{-|
+Inserts an element into a 'BinaryTree'.
+
+* If it finds a leaf, insert there
+
+* If smaller than the item on the node, insert into the left.
+
+* If larger then the item on the node, insert to the right.
+>>> treeInsert 1 Leaf
+Node 1 Leaf Leaf
+-}
 treeInsert :: Ord a => a -> BinaryTree a -> BinaryTree a
 treeInsert x Leaf = Node x Leaf Leaf
-treeInsert _ _ = undefined
+treeInsert x (Node y l r) | x <= y  = Node y (treeInsert x l) r
+                          | otherwise = Node y l (treeInsert x r)
+
+
+
+
+treeMerge :: Ord a => BinaryTree a -> BinaryTree a -> BinaryTree a
+treeMerge t Leaf = t
+treeMerge t (Node x l r ) = treeInsert x $ treeMerge (treeMerge t l ) r 
