2e911e504bf62d8754639e8dcefd7504ff74a3b7
Haroon Khan <hkhan@healthexpense.com>
2018-02-06 20:45:48 -0800

    Complete Section on HUnit

--- projecthaskell/chapter15/src/Chapter15/BinaryTrees.hs
+++ projecthaskell/chapter15/src/Chapter15/BinaryTrees.hs
@@ -22,9 +22,30 @@ treeInsert x Leaf = Node x Leaf Leaf
 treeInsert x (Node y l r) | x <= y  = Node y (treeInsert x l) r
                           | otherwise = Node y l (treeInsert x r)
 
-
+treeFind :: Ord a => a -> BinaryTree a -> Maybe a
+treeFind e (Node v lt rt) = case compare e v of
+                              EQ -> Just v
+                              LT -> treeFind e lt
+                              GT -> treeFind e rt
 
 
 treeMerge :: Ord a => BinaryTree a -> BinaryTree a -> BinaryTree a
 treeMerge t Leaf = t
-treeMerge t (Node x l r ) = treeInsert x $ treeMerge (treeMerge t l ) r 
+treeMerge t (Node x l r ) = treeInsert x $ treeMerge (treeMerge t l ) r
+
+treeSize :: BinaryTree a -> Integer
+treeSize t = let treeSizeRT :: Num n => BinaryTree a -> n -> n
+                 treeSizeRT Leaf n = n
+                 treeSizeRT (Node _ lt rt) n = let ltSize = treeSizeRT lt n
+                                                   rtSize = treeSizeRT rt n
+                                               in 1 + ltSize + rtSize
+             in
+               treeSizeRT t 0
+
+treeRemove :: (Eq a, Ord a) => a -> BinaryTree a -> BinaryTree a
+treeRemove _ Leaf = Leaf
+treeRemove e (Node a lt rt) = if e==a
+                              then treeMerge lt rt
+                              else treeInsert a $ treeMerge ltn rtn
+  where ltn = treeRemove e lt
+        rtn = treeRemove e rt
--- projecthaskell/chapter15/test/Tasty.hs
+++ projecthaskell/chapter15/test/Tasty.hs
@@ -3,14 +3,50 @@ module Main where
 import Test.Tasty
 import Test.Tasty.HUnit as HU
 import Chapter15.BinaryTrees
+import Data.Maybe
+
 
 hunitTestInsertOnLeaf :: TestTree
-hunitTestInsertOnLeaf = HU.testCase "Insert 'a' on empt tree" $
+hunitTestInsertOnLeaf = HU.testCase "Insert 'a' on empty tree" $
   assertEqual "Insertion is wrong" (treeInsert 'a' Leaf) (Node 'a' Leaf Leaf)
+
+hunitTestInsertOnLeaf' = HU.testCase "Insert 'a' on empty tree" $
+  treeInsert 'a' Leaf HU.@?= Node 'a' Leaf Leaf
+
+hunitTestInsertOnLeaf'' = HU.testCase "Insert 'a' on empty tree" $
+  Node 'a' Leaf Leaf HU.@?= treeInsert 'a' Leaf
+
+-- Using an equality property
+
+hunitTestInsertFind :: Ord a => a -> BinaryTree a -> TestTree
+hunitTestInsertFind e t = HU.testCase "Insert can be found" $
+  assertBool "Cannot find element" (isJust $ treeFind e $ treeInsert e t)
+
+-- Excercise 15-1: Unit Testing Binary Trees
+hunitTestIncNodeCount :: Ord a => a -> BinaryTree a -> TestTree
+hunitTestIncNodeCount e t = HU.testCase "Increment Node Count when a node is inserted" $
+  assertBool "Node Count not expected" ((1 + treeSize t) == (treeSize $ treeInsert e t))
+
+hunitTestDecNodeCount :: (Ord a) => a -> BinaryTree a -> TestTree
+hunitTestDecNodeCount e t = HU.testCase "Decrement Node Count when a node is removed" $
+  assertBool "Node Count not expected" ( (treeSize t) - 1  == (treeSize $ treeRemove e t))
+
+hunitTestNodeCountisZeroOnLeaf :: (Ord a) => a -> BinaryTree a -> TestTree
+hunitTestNodeCountisZeroOnLeaf e t = HU.testCase "Node Count is 0 when a node is removed from an empty tree" $
+  assertBool "Node Count is not 0" (0 == (treeSize $ treeRemove e t))
 
 allTests::TestTree
 allTests = testGroup "Tasty Tests" [
-  testGroup "HUnit Tests" [ hunitTestInsertOnLeaf ]
+  testGroup "HUnit Tests" [ hunitTestInsertOnLeaf
+                          , hunitTestInsertOnLeaf'
+                          , hunitTestInsertOnLeaf''
+                          , hunitTestInsertFind 'b' Leaf
+                          , hunitTestInsertFind 'c' (Node 'd' Leaf Leaf)
+                          , hunitTestIncNodeCount 'a' Leaf
+                          , hunitTestIncNodeCount 'b' (Node 'a' Leaf Leaf)
+                          , hunitTestDecNodeCount 'a' (Node 'a' Leaf Leaf)
+                          , hunitTestNodeCountisZeroOnLeaf 'a' Leaf
+                          ]
   ]
 
 
