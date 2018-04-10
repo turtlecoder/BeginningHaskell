55cb80d46a952840fbcc7f4c5d6de2de9caa451c
Haroon Khan <hkhan@healthexpense.com>
2018-04-10 07:43:28 -0700

    Exercise 4-4: The Same Client

--- projecthaskell/chapter04/src/Chapter04/Client.hs
+++ projecthaskell/chapter04/src/Chapter04/Client.hs
@@ -18,11 +18,11 @@ data Client i = GovOrg { clientId :: i
                 | Individual { clientId :: i
                              , person :: Person
                              }
-                deriving (Show, Eq, Ord)
+                deriving (Show, Ord)
 
 data Person = Person { firstName :: String
                      , lastName :: String
-                     } deriving (Show, Eq, Ord)
+                     } deriving (Show, Ord)
 
 -- Excercise 4-3
 -- Classify Clients Solution a
@@ -40,6 +40,7 @@ classifyClientsA clients =
     L.foldr addToMap emptyMap clients
 
 
+
 -- classify clients by partition
 classifyClientsB :: Ord i => [Client i] -> Map ClientKind (Set (Client i))
 classifyClientsB clients = let (a, b, c) = L.foldr appendListsFn ([],[],[]) clients
@@ -50,3 +51,18 @@ classifyClientsB clients = let (a, b, c) = L.foldr appendListsFn ([],[],[]) clie
                              M.fromList [ (GovOrgKind, S.fromList a)
                                         , (CompanyKind, S.fromList b)
                                         , (IndividualKind, S.fromList c)]
+
+
+-- Excercise 4-5: The Same Client
+instance (Eq i ) =>Eq (Client i) where
+  (==) (GovOrg { clientId=cid1, clientName = cn1 }) (GovOrg { clientId = cid2 , clientName = cn2 }) = cid1 == cid2 && cn1==cn2
+  (==)
+    (Company { clientId = cid1, clientName = co1 })
+    (Company { clientId = cid2 , clientName = co2}) = cid1==cid2 && co1 == co2
+  (==)
+    (Individual { clientId = cid1, person = p1 })
+    (Individual { clientId = cid2, person = p2 }) = cid1 == cid2 && p1 == p2
+  (==) _ _ = undefined
+
+instance Eq Person where
+  (==) (Person { firstName = fn1, lastName = ln1 }) (Person { firstName = fn2, lastName = ln2 }) = fn1==fn2 && ln1==ln2
