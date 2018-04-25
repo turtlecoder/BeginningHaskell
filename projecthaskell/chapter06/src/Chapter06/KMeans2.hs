39fc0041a7528b19f0fac6069a96231a82a166a1
Haroon Khan <hkhan@healthexpense.com>
2018-04-24 17:32:06 -0700

    Update to KMeans2.hs

--- projecthaskell/chapter06/src/Chapter06/KMeans2.hs
+++ projecthaskell/chapter06/src/Chapter06/KMeans2.hs
@@ -2,6 +2,7 @@
 module Chapter06.KMeans2 where
 
 import Control.Lens
+import Chapter06.KMeansCommon
 
 data KMeansState e v = KMeansState { _centroids::[v]
                                    , _points::[e]
@@ -11,3 +12,13 @@ data KMeansState e v = KMeansState { _centroids::[v]
                                    }
 
 makeLenses ''KMeansState
+
+vectorInitFunc :: VectorInitFunc (Double, Double) (Double, Double)
+vectorInitFunc _ _ = undefined
+
+-- Initialize the State function
+initializeState :: (VectorInitFunc e v) -> Int -> [e] -> Double -> KMeansState e v
+initializeState initFn n pts threshold = KMeansState (initFn n pts) pts (1.0/0.0) threshold 0
+
+kMeans::(Vector v, Vectorizable e v) => (VectorInitFunc e v) -> Int -> [e] -> Double -> [v]
+kMeans _ _ _ = undefined
