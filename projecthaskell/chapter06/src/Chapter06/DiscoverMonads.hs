3a35b0808e35e3e803e81f124a1eb0d84d123ddd
Haroon Khan <hkhan@healthexpense.com>
2018-04-27 09:28:11 -0700

    Add Demonstration Code for Discovering Monads

--- projecthaskell/chapter06/src/Chapter06/DiscoverMonads.hs
+++ projecthaskell/chapter06/src/Chapter06/DiscoverMonads.hs
@@ -1,1 +1,26 @@
 module Chapter06.DiscoverMonads where
+
+import Data.Maybe
+
+
+
+meanPurchase :: id -> Double
+meanPurchase clientId = let purchaseIds = purchasesByClientId clientId
+                            in foldr (+) 0.0 $ catMaybes $ map purchaseValue purchaseIds
+
+purchasesByClientId :: clientId -> [purchaseId]
+purchasesByClientId clientId = undefined
+
+numberOfItemsByPurchaseId:: purchaseId -> Maybe n
+numberOfItemsByPurchaseId purchaseId = undefined
+
+purchaseValue :: purchaseId -> Maybe Double
+purchaseValue purchaseId = case numberOfItemsByPurchaseId purchaseId of
+                             Nothing -> Nothing
+                             Just n -> case productIdByPurchaseId purchaseId of
+                                         Nothing -> Nothing
+                                         Just price -> Just $ (fromInteger n) * price
+
+  
+productIdByPurchaseId::purchaseId -> Maybe productId
+productIdByPurchaseId purchaseId = undefined
