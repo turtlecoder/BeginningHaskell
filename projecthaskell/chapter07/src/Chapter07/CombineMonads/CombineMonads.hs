1983a3dbb453002d338042e328736de90155e940
Haroon Khan <hkhan@healthexpense.com>
2018-06-05 08:25:40 -0700

    More on Combining Monads

--- projecthaskell/chapter07/src/Chapter07/CombineMonads/CombineMonads.hs
+++ projecthaskell/chapter07/src/Chapter07/CombineMonads/CombineMonads.hs
@@ -1,1 +1,17 @@
 module Chapter07.CombineMonads.CombineMonads where
+
+import Control.Monad
+import Control.Monad.Writer
+
+pathsWriter :: [(Int, Int)] -> Int -> Int -> [[Int]]
+pathsWriter edges start end = map execWriter (pathsWriter' edges start end)
+
+pathsWriter' :: [(Int, Int)] -> Int -> Int -> [Writer [Int] () ]
+pathsWriter' edges start end =
+  let e_paths = do (e_start, e_end) <- edges
+                   guard $ e_start == end
+                   subpath <- pathsWriter' edges e_end end
+                   return $ do tell [start]
+                               subpath
+  in if start == end then tell [start] : e_paths else e_paths
+    
