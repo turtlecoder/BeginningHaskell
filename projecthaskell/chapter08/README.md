747a8d51b09e3017fe2b058e6cb7dbc81ae822c1
Haroon Khan <hkhan@healthexpense.com>
2018-06-19 22:41:40 -0700

    Complete Excercise 8-3

--- projecthaskell/chapter08/README.md
+++ projecthaskell/chapter08/README.md
@@ -3,7 +3,7 @@
 - Excercise 8-1: Parallel K-Means implementation, based on Chapter 6 code.
   Not the best implementation. It may not even run, need some better text 
   fixtures to test the code. 
-  - [ ] _Add Link here_
+  - [ ] _Add Link here in ParallelizingApriori.hs_
   
 - Software Transactional Memory
   - Concurrent Usage of Resources
@@ -13,4 +13,7 @@
 	- if pure, tehen all ordering will have same result. 
 	
 - Excercise 8-2: Implement a function for use with STM
- - [ ] _Add Link here_
+ - [ ] _Add Link here RollingBackTransactions.hs
+ 
+ - Excercise 8-3: Queuing Travellers
+   - [ ] _Add Link here ProducerConsumers.hs__
--- projecthaskell/chapter08/src/Chapter08/SoftwareTransactionalMemory/ProducerConsumerQueues.hs
+++ projecthaskell/chapter08/src/Chapter08/SoftwareTransactionalMemory/ProducerConsumerQueues.hs
@@ -1,1 +1,51 @@
 module Chapter08.SoftwareTransactionalMemory.ProducerConsumerQueues where
+
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.STM.TQueue
+import Control.Concurrent.STM.TBQueue
+import GHC.Conc
+import Prelude hiding (product)
+import Chapter08.SoftwareTransactionalMemory.RollingBackTransactions
+import System.Random
+
+mainFn :: IO ()
+mainFn = do q <- newTQueueIO
+            _ <-forkIO $ backend q
+            replicateM_ 10 $ forkIO $ frontend q
+            _ <- getLine
+            return ()
+
+backend :: TQueue (String, Integer) -> IO ()
+backend q = do m <- newTVarIO 10000
+               s <- newTVarIO [("a", 7)]
+               forever $ atomically $ do (product, price) <- readTQueue q
+                                         pay product price m s
+
+frontend :: TQueue (String, Integer) -> IO ()
+frontend q = do (product, price) <- undefined -- get purchase information from client
+                atomically $ writeTQueue q (product, price)
+               
+
+-- Excercise 8-3: Queuing Travellers
+-- Implementing using TBQueue
+
+requestTM :: TBQueue Integer -> Integer -> IO ()
+requestTM tmQueue dstYear = atomically $ writeTBQueue tmQueue dstYear
+
+
+travelByQTM :: TBQueue Integer -> IO ()
+travelByQTM tbQueue = do delay <- atomically $ readTBQueue tbQueue
+                         threadDelay (fromIntegral $ delay * 100000)
+
+forkCustomer :: TBQueue Integer -> IO ()
+forkCustomer tmq = do dstYear <- randomRIO (1999, 2018)
+                      requestTM tmq dstYear
+                  
+
+mainTMLoop :: IO ()
+mainTMLoop = do tmq <- newTBQueueIO 10
+                replicateM_ 100 $ forkIO $ forkCustomer tmq
+                forever $ travelByQTM tmq
+                                       
+                
