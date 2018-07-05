f9624b948340db692859f1c3585d6a726d5e22b8
Haroon Khan <hkhan@healthexpense.com>
2018-07-05 07:21:52 -0700

    Finish 2nd Chapter of Cloud Haskell tutorial

--- cloud-haskell/cloud-topology/src-p2p/Main.hs
+++ cloud-haskell/cloud-topology/src-p2p/Main.hs
@@ -3,6 +3,6 @@ import           Control.Monad.Trans (liftIO)
 import           Control.Concurrent (threadDelay)
 import Control.Distributed.Process.Node (initRemoteTable)
 
-main = P2P.bootstrap "127.0.0.1" "9001" (\_ -> ("127.0.0.1", "9001")) initRemoteTable [P2P.makeNodeId "seedhost:9000"] $ do
-  liftIO $ threadDelay 1000000 -- give dispatcher a second to discover other nodes
+main = P2P.bootstrap "127.0.0.1" "9001" (\_ -> ("127.0.0.1", "9000")) initRemoteTable [P2P.makeNodeId "seedhost:9001"] $ do
+  liftIO $ threadDelay 5000000 -- give dispatcher a second to discover other nodes
   P2P.nsendPeers "myService" ("some", "message")
