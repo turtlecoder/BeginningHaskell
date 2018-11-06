05743aaba67f9704110be5b4ff9617d3d0700779
Haroon Khan <hkhan@healthexpense.com>
2018-11-05 22:23:02 -0800

    Finish section on ConcurrentResources

---
+++ projecthaskell/chapter08/chapter08-stm-random-updates/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Chapter08.STM.ConcurrentResources
+
+main :: IO ()
+main = mainRandomUpdatesReads
--- projecthaskell/chapter08/src/Chapter08/STM/ConcurrentResources.hs
+++ projecthaskell/chapter08/src/Chapter08/STM/ConcurrentResources.hs
@@ -1,10 +1,14 @@
 module Chapter08.STM.ConcurrentResources where
 
 import Control.Concurrent
+import System.Random
+import Control.Monad
+  
 
 updateMoney::MVar Integer -> IO ()
 updateMoney v = do m <- takeMVar v
-                   putStrLn $ "Updating Value, which is " ++ show m
+                   tid <- myThreadId
+                   putStrLn $ "Thread ID: " ++ show tid ++ " Updating Value, which is " ++ show m
                    putMVar v (m + 500)
 
 
@@ -18,3 +22,28 @@ mainUpdateMoney = do v <- newMVar 10000
                      putStrLn $ "Creating Thread 3: " ++ show t3
                      _ <- getLine
                      return ()
+
+
+readMoney :: MVar Integer -> IO ()
+readMoney v = do m <- readMVar v
+                 tid <- myThreadId
+                 putStrLn $ "Thread ID: " ++ show tid ++ " The current Value is " ++ show m
+
+randomDelay :: IO ()
+randomDelay = do r <- randomRIO (3, 15)
+                 threadDelay (r*1000000)
+
+
+-- Create a forkdelay function, to spawn n threads with a random waiting time before
+forkDelay :: Int -> IO () -> IO ()
+forkDelay n f = replicateM_ n $ do tid <- forkIO (randomDelay >> f )
+                                   putStrLn $ "Creating Thread ID: " ++ show tid
+                                   return ()
+                 
+
+mainRandomUpdatesReads :: IO ()
+mainRandomUpdatesReads  = do v <- newMVar 10000
+                             forkDelay 5 $ updateMoney v
+                             forkDelay 5 $ readMoney v
+                             _ <- getLine
+                             return ()
