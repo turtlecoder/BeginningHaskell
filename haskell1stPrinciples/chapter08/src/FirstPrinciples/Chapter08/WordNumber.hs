7b3adb4465cfdc2771b66c0fa4cc066b5bb60b75
Haroon Khan <hkhan@healthexpense.com>
2018-11-18 18:27:01 -0800

    Add a new module for WordsNumber with new code
    Also added dependencies, and cleaned up whitespace.

--- haskell1stPrinciples/chapter08/src/FirstPrinciples/Chapter08/WordNumber.hs
+++ haskell1stPrinciples/chapter08/src/FirstPrinciples/Chapter08/WordNumber.hs
@@ -1,1 +1,35 @@
+{-# LANGUAGE DataKinds #-}
+
 module FirstPrinciples.Chapter08.WordNumber where
+
+import Data.List (intersperse)
+import Refined
+
+-- Converts a single digit 
+digitToWord :: Int  -> String
+digitToWord n = let
+  digitList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  in
+  digitList !! n
+
+-- Converts 
+digits :: Refined Positive Int -> [Int]
+digits n = let
+  goDigits :: Int -> [Int] -> [Int]
+  goDigits 0 []    = [0]
+  goDigits 0 accum = accum
+  goDigits n accum = let
+    divR = n `div` 10
+    modR = n `mod` 10
+    in
+    goDigits divR (modR:accum)
+  in
+  goDigits (unrefine n) []
+
+
+wordNumber :: Int -> Either String String
+wordNumber n = do
+  number <- (refine n )::(Either String (Refined Positive Int))
+  digitList <- Right (digits number)
+  let wordList = concat $ intersperse "-" $ fmap digitToWord digitList
+  return wordList
