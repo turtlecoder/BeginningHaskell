fac40cc8b4b50585751b0048f4290ff26d67c491
Haroon Khan <haroon.khan@gmail.com>
2020-02-22 19:39:49 -0800

    Finishing Chapter 05

--- projecthaskell/chapter05/src/Chapter05/Infinity.hs
+++ projecthaskell/chapter05/src/Chapter05/Infinity.hs
@@ -1,6 +1,7 @@
-module Chapter04.Infinity where
+module Chapter05.Infinity where
 
 import Data.List
+import Debug.Trace
 
 data TimeMachine = TM { _manufacturer :: String
                       , _year :: Integer } deriving (Eq, Show)
@@ -14,10 +15,19 @@ timelyIncMachines = timeMachinesFrom "Timely Inc." 100
 
 threeTimeMachines = take 3 timelyIncMachines
 
+-- >>> :t repeat
+-- repeat :: a -> [a]
+
+-- >>> take 3 $ repeat $ TM "Timely Inc." 2020
+-- [TM {_manufacturer = "Timely Inc.", _year = 2020},TM {_manufacturer = "Timely Inc.", _year = 2020},TM {_manufacturer = "Timely Inc.", _year = 2020}]
+
 findMachines = find (\TM { _year = y } -> y > 2018) timelyIncMachines
 
 
 zipWithNumbers = (\list -> zip [ 1 .. length list] list) "abcde"
+
+-- >>> zipWithNumbers
+-- [(1,'a'),(2,'b'),(3,'c'),(4,'d'),(5,'e')]
 
 
 allNumbers = allNumbersFrom 1
@@ -31,6 +41,9 @@ thirdZipNumbers = zip [ 1 .. ] "abcde"
 fibonacci :: [Integer]
 fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
 
+-- >>> :t zipWith
+-- zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+
 
 _20thfibonacci = fibonacci !! 20
 
@@ -43,23 +56,62 @@ specialOffer = cycle [ TM m 2005, TM m 1994, TM m 1998]
   where
     m = "Timely Inc."
 
+-- >>> :t cycle
+-- cycle :: [a] -> [a]
+
 
 take4SpecialOffer = take 4 specialOffer
+-- >>> take4SpecialOffer
+-- [TM {_manufacturer = "Timely Inc.", _year = 2005},TM {_manufacturer = "Timely Inc.", _year = 1994},TM {_manufacturer = "Timely Inc.", _year = 1998},TM {_manufacturer = "Timely Inc.", _year = 2005}]
 
 
 fibonacci2 = map fst $ iterate (\(n, n') -> (n', n+n')) (0, 1)
 
-_20thFibonacci2 = fibonacci2 !! 20
+-- >>> :t iterate
+-- iterate :: (a -> a) -> a -> [a]
 
--- Excercise 5-1: The sieve of Eratosthenese
+_20thFibonacci2 = fibonacci2 !! 20
+-- >>> _20thFibonacci2
+-- 6765
+
+-- Excercise 5-1: The sieve of Erosthenese
 primes :: [Integer]
-primes = sieve [2..]
+primes = sieve [2 ..]
   where
-    sieve (p:xs) = p : sieve (minus xs [p, p+p ..])
-    minus (x:xs) (y:ys) = case compare x y of
-                            LT -> x:minus xs (y:ys)
-                            EQ -> minus xs ys
-                            GT -> minus (x:xs) ys
-    minus a _ = a
+    sieve (p:xs) =  p : sieve (minus xs [p, p + p ..])
+
+minus (x:xs) (y:ys) = 
+  case compare x y of
+     LT -> x:minus xs (y:ys)
+     EQ -> minus xs ys
+     GT -> minus (x:xs) ys
+               
+minus xs _ = xs
                              
+-- >>> take 2 primes 
+-- [2,3]
+
+
+-- >>> :type-at Chapter05.Infinity 76 76 76 76 minus
+-- minus :: forall a. Ord a => [a] -> [a] -> [a]
+
+-- >>> :type-at Chapter05.Infinity 72 72 72 72 primes
+-- primes :: [Integer]
+
+
+-- >>> 2
+-- 2
+
+-- >>> :t fibonacci
+-- fibonacci :: [Integer]
+
+-- >>> :t (!!)
+-- (!!) :: [a] -> Int -> a
+
+-- >>> (repeat 1 ) !! 10
+-- 1
+
+-- >>> :t repea
+-- repeat :: a -> [a]
+
 
